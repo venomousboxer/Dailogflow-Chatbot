cf09f4f48ac59e1d07684e9c5e445ac5ea2a11c7
venomousboxer <y.mohit03031999@gmail.com>
2019-01-15 17:21:57 +0530

    more changes

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,10 +2,6 @@ module.exports = (req,res)=>{
     const hubChallenge = req.query['hub.challenge']
     const hubMode = req.query['hub.mode']
     const verifyTokenMatches = (req.query['hub.verify_token']==='crowdbotics')
-    
-    // const hubChallenge = req.query[‘hub.challenge’];
-    // const hubMode = req.query[‘hub.mode’];
-    // const verifyTokenMatches = (req.query[‘hub.verify_token’] === ‘crowdbotics’);
 
     if(hubMode&&verifyTokenMatches){
         res.status(200).send(hubChallenge)
